a03ad55bf92466187319c2505322a9237960fd70
Charles <xuchi.int@gmail.com>
2023-02-12 18:59:06 -0800

    update cmpBGSubtractor.py

--- cmpBGSubtractor.py
+++ cmpBGSubtractor.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Video Capture
 capture = cv2.VideoCapture("D:/sonar/2020-05-27_071000.mp4")
-capture = cv2.VideoCapture("D:/sonar/2020-05-24_000000.mp4")
+#capture = cv2.VideoCapture("D:/sonar/2020-05-24_000000.mp4")
 """
 history is the number of frames used to build the statistic model of the background. 
 The smaller the value is, the faster changes in the background will be taken into account 
@@ -13,16 +13,19 @@ dist2Threshold is a threshold to define whether a pixel is different from the ba
 The smaller the value is, the more sensitive movement detection is. And vice versa.
 
 detectShadows : If set to true, shadows will be displayed in gray on the generated mask. (Example bellow)
+
+
+count: https://stackoverflow.com/questions/52087533/how-to-find-number-of-clusters-in-a-image
 """
 
 # Subtractors
 # history = 300, nmixtures = 5, backgroundRatio = 0.001
-mogSubtractor = cv2.bgsegm.createBackgroundSubtractorMOG(100)
+mogSubtractor = cv2.bgsegm.createBackgroundSubtractorMOG(100)       # 1
 # history = 300, varThreshold = 16, detectShadows = true
-mog2Subtractor = cv2.createBackgroundSubtractorMOG2(100, 40, False)
+mog2Subtractor = cv2.createBackgroundSubtractorMOG2(100, 40, False) # 3
 gmgSubtractor = cv2.bgsegm.createBackgroundSubtractorGMG(10, .8)    #
-knnSubtractor = cv2.createBackgroundSubtractorKNN(100, 400, True)   #
-cntSubtractor = cv2.bgsegm.createBackgroundSubtractorCNT(5, True)   # good
+knnSubtractor = cv2.createBackgroundSubtractorKNN(100, 400, True)   # 2
+cntSubtractor = cv2.bgsegm.createBackgroundSubtractorCNT(5, True)   #
 
 # Keeps track of what frame we're on
 frameCount = 0
@@ -45,8 +48,26 @@ while (1):
     # Resize the frame
     resizedFrame = cv2.resize(frame, (0, 0), fx=0.64, fy=0.64)
 
+
+
     # Get the foreground masks using all of the subtractors
     mogMask = mogSubtractor.apply(resizedFrame)
+    countMogMask = mogMask.copy()
+    ret, thresh = cv2.threshold(countMogMask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh)
+    print("n_labels: " + str(n_labels))
+
+    size_thresh = 1
+    for i in range(1, n_labels):
+        if stats[i, cv2.CC_STAT_AREA] >= size_thresh:
+            # print(stats[i, cv2.CC_STAT_AREA])
+            x = stats[i, cv2.CC_STAT_LEFT]
+            y = stats[i, cv2.CC_STAT_TOP]
+            w = stats[i, cv2.CC_STAT_WIDTH]
+            h = stats[i, cv2.CC_STAT_HEIGHT]
+            cv2.rectangle(countMogMask, (x, y), (x + w, y + h), (0, 255, 0), thickness=1)
+
+
     mog2Mmask = mog2Subtractor.apply(resizedFrame)
     gmgMask = gmgSubtractor.apply(resizedFrame)
     gmgMask = cv2.morphologyEx(gmgMask, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
@@ -59,9 +80,6 @@ while (1):
     gmgCount = np.count_nonzero(gmgMask)
     knnCount = np.count_nonzero(knnMask)
     cntCount = np.count_nonzero(cntMask)
-
-    #cv2.findContours
-    cv2.threshold(mogMask, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU, mogMask)
 
     print('mog Frame: %d, Pixel Count: %d' % (frameCount, mogCount))
     print('mog2M Frame: %d, Pixel Count: %d' % (frameCount, mog2MCount))
@@ -77,28 +95,24 @@ while (1):
     cv2.putText(knnMask, 'KNN', titleTextPosition, cv2.FONT_HERSHEY_SIMPLEX, titleTextSize, textColor, 2, cv2.LINE_AA)
     cv2.putText(cntMask, 'CNT', titleTextPosition, cv2.FONT_HERSHEY_SIMPLEX, titleTextSize, textColor, 2, cv2.LINE_AA)
 
-    stealingTextPosition = (100, 100)
+    countTextPosition = (100, 100)
     if (frameCount > 1):
         if (mogCount > movementCount):
-            print(movementText)
-            cv2.putText(mogMask, 'Someones stealing your honey', stealingTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
+            cv2.putText(mogMask, 'count:' + str(1), countTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
                         textColor, 2, cv2.LINE_AA)
         if (mog2MCount > movementCount):
-            print(movementText)
-            cv2.putText(mog2Mmask, 'Someones stealing your honey', stealingTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
+            cv2.putText(mog2Mmask, 'count:' + str(1), countTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
                         textColor, 2, cv2.LINE_AA)
         if (gmgCount > movementCount):
-            print(movementText)
-            cv2.putText(gmgMask, 'Someones stealing your honey', stealingTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
+            cv2.putText(gmgMask, 'count:' + str(1), countTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
                         textColor, 2, cv2.LINE_AA)
         if (knnCount > movementCount):
-            print(movementText)
-            cv2.putText(knnMask, 'Someones stealing your honey', stealingTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
+            cv2.putText(knnMask, 'count:' + str(1), countTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
                         textColor, 2, cv2.LINE_AA)
         if (cntCount > movementCount):
-            print(movementText)
-            cv2.putText(cntMask, 'Someones stealing your honey', stealingTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
+            cv2.putText(cntMask, 'count:' + str(1), countTextPosition, cv2.FONT_HERSHEY_SIMPLEX, .5,
                         textColor, 2, cv2.LINE_AA)
+
 
     cv2.imshow('Original', resizedFrame)
     cv2.imshow('MOG', mogMask)
@@ -107,12 +121,16 @@ while (1):
     cv2.imshow('KNN', knnMask)
     cv2.imshow('CNT', cntMask)
 
+    cv2.imshow('countMogMask', countMogMask)
+
     cv2.moveWindow('Original', 0, 0)
     cv2.moveWindow('MOG', 400, 0)
     cv2.moveWindow('KNN', 800, 0)
     cv2.moveWindow('GMG', 1200, 0)
     cv2.moveWindow('MOG2', 1600, 0)
     cv2.moveWindow('CNT', 2000, 0)
+
+    cv2.moveWindow('countMogMask', 2400, 0)
 
     k = cv2.waitKey(0) & 0xff
     print(k)
