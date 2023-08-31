f6169f8a094d3bff4677cbd4708ab04bf2918f94
Henry <henry@lab.com>
2023-08-30 17:25:48 -0700

    add function to segment the COCO annotation bounding box

--- annotation_converter.py
+++ annotation_converter.py
@@ -1,6 +1,7 @@
 import re
 import cv2
 import csv
+import json
 import time
 import pyARIS
 import threading
@@ -74,7 +75,52 @@ def convert_annotation_to_coco():
     
     print("\n\nDone!")
 
-# TODO: segment the COCO annotation file into 3 segments
+# bounding box format: [top left x position, top left y position, width, height]
+# crop_boundaries = [[0:462], [462:890], [890:]]
+# Go though the annotation file and split the annotation into 3 files according to the crop_boundaries
+# If the bounding box is in between segments, the bounding box is simply discarded
+def reconstruct_coco_annotation(file_path, folder_path):
+    with open(file_path, "r") as f:
+        data = json.load(f)
+        temp_annotation_list = []
+        for _ in range(3):
+            temp_annotation_list.append([])
+
+        for idx, obj in enumerate(data["annotations"]):
+            ymin = obj["bbox"][1]
+            ymax = obj["bbox"][1] + obj["bbox"][3]
+            # print(obj["bbox"])
+            # print("ymin: %d, ymax: %d" %(ymin, ymax))
+
+            if ymax < 462:
+                temp_annotation_list[0].append(obj)
+            elif ymin >= 462 and ymax < 890:
+                temp_annotation_list[1].append(obj)
+            elif ymin >= 890:
+                temp_annotation_list[2].append(obj)
+
+        for i in range(3):
+            tempJson = data.copy()
+            # ! after converting the annotation file to COCO format, some of the annotation id is duplicated for same reason
+            # ! Manually reassign the id to avoid this problem
+            for idx, obj in enumerate(temp_annotation_list[i]):
+                obj["id"] = idx + 1
+            tempJson["annotations"] = temp_annotation_list[i]
+
+            with open(os.path.join(folder_path, "instances_default_%d.json" %(i)), "w") as json_file:
+                json.dump(tempJson, json_file)
+
+def segment_coco_annotation():
+    coco_proj_path = os.path.join(outputDir, "coco_proj")
+
+    for item in os.listdir(coco_proj_path):
+        if os.path.isdir(os.path.join(coco_proj_path, item)):
+            annotation_folder_path = os.path.join(coco_proj_path, item, "annotations")
+            annotation_file_path = os.path.join(annotation_folder_path, "instances_default.json")
+
+            reconstruct_coco_annotation(annotation_file_path, annotation_folder_path)
+
 if __name__ == "__main__":
-    preprocess_data_and_generate_csv_file()
-    convert_annotation_to_coco()
+    # preprocess_data_and_generate_csv_file()
+    # convert_annotation_to_coco()
+    segment_coco_annotation()
