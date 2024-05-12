b8a406f5a6dc9215f2ea3ac3518f184bb53926a5
Charles <xuchi.int@gmail.com>
2024-05-11 22:16:38 -0700

    on another server

--- sonar_direct_on_MOG.py
+++ sonar_direct_on_MOG.py
@@ -4,10 +4,13 @@ import time
 import math
 
 # Video Capture
-capture = cv2.VideoCapture("output/Haida_2020-05-24/2020-05-24_230000_373-443.mp4")
-# capture = cv2.VideoCapture("./output/Haida_2020-05-24/2020-05-24_000000_-8-62.mp4")
-# capture = cv2.VideoCapture("D:/sonar/2020-05-25_020000.mp4")
-# capture = cv2.VideoCapture("D:/sonar/2020-05-24_000000.mp4")
+capture = cv2.VideoCapture("./output/Haida_2020-05-24/2020-05-24_015000_503-573.mp4")
+#capture = cv2.VideoCapture("./output/Haida_2020-05-24/2020-05-24_230000_373-443.mp4")
+capture = cv2.VideoCapture("./output/Haida_2020-05-24/haida23.mp4")
+
+#capture = cv2.VideoCapture("./output/Haida_2020-05-24/2020-05-24_000000_-8-62.mp4")
+#capture = cv2.VideoCapture("D:/sonar/2020-05-25_020000.mp4")
+#capture = cv2.VideoCapture("D:/sonar/2020-05-24_000000.mp4")
 """
 
 MOG algorithm brief introduction (in chinese) https://blog.csdn.net/weixin_53598445/article/details/124093067
@@ -26,9 +29,9 @@ count: https://stackoverflow.com/questions/52087533/how-to-find-number-of-cluste
 
 # Subtractors
 # history = 300, nmixtures = 5, backgroundRatio = 0.001
-mogSubtractor = cv2.bgsegm.createBackgroundSubtractorMOG(100)  # 1
+mogSubtractor = cv2.bgsegm.createBackgroundSubtractorMOG(100)       # 1
 # history = 300, varThreshold = 16, detectShadows = true
-mog2Subtractor = cv2.createBackgroundSubtractorMOG2(100, 40, True)  # 3
+mog2Subtractor = cv2.createBackgroundSubtractorMOG2(100, 40, True) # 3
 
 # Keeps track of what frame we're on
 frameCount = 0
@@ -50,12 +53,14 @@ while True:
     print("FrameCount: " + str(frameCount))
 
     # Resize the frame
-    resizedFrame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+    resizedFrame = cv2.resize(frame, (0, 0), fx=0.7, fy=0.7)
+    height, weight, _ = resizedFrame.shape
 
     # Get the foreground masks using all the subtractors
     mogMask = mogSubtractor.apply(resizedFrame)
     countMOGMask = mogMask.copy()
     morphMOGMask = cv2.morphologyEx(countMOGMask, cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2)))
+
     ret, thresh = cv2.threshold(countMOGMask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     n_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(thresh)
     print("n_labels: " + str(n_labels))
@@ -64,15 +69,16 @@ while True:
     size_thresh = 3
     for i in range(1, n_labels):
         if stats[i, cv2.CC_STAT_AREA] >= size_thresh:
-            # print(stats[i, cv2.CC_STAT_AREA])
+            #print(stats[i, cv2.CC_STAT_AREA])
             x = stats[i, cv2.CC_STAT_LEFT]
             y = stats[i, cv2.CC_STAT_TOP]
             w = stats[i, cv2.CC_STAT_WIDTH]
             h = stats[i, cv2.CC_STAT_HEIGHT]
-            print("loc: " + str(x) + " " + str(y) + " " + str(w) + " " + str(h))
-            region = resizedFrame[y:y + h, x:x + w]
+            print("loc: " + str(x) + " " +str(y) + " " + str(w) + " " + str(h))
+            region = resizedFrame[y:y+h, x:x+w]
             mean_color = np.mean(region, axis=(0, 1))
-            if np.mean(mean_color) > 100:
+            print(y)
+            if np.mean(mean_color)*y/height > 60 and w*h > 10:
                 cv2.rectangle(countMOGMask, (x, y), (x + w, y + h), (0, 255, 0), thickness=1)
                 cv2.rectangle(resizedFrame, (x, y), (x + w, y + h), (0, 255, 0), thickness=1)
 
@@ -88,24 +94,20 @@ while True:
     titleTextPosition = (100, 40)
     titleTextSize = 1.2
     cv2.putText(mogMask, 'MOG', titleTextPosition, cv2.FONT_HERSHEY_SIMPLEX, titleTextSize, textColor, 2, cv2.LINE_AA)
-    cv2.putText(mog2Mmask, 'MOG2', titleTextPosition, cv2.FONT_HERSHEY_SIMPLEX, titleTextSize, textColor, 2,
-                cv2.LINE_AA)
+    cv2.putText(mog2Mmask, 'MOG2', titleTextPosition, cv2.FONT_HERSHEY_SIMPLEX, titleTextSize, textColor, 2, cv2.LINE_AA)
 
     countTextPosition = (100, 100)
 
     cv2.imshow('Original', resizedFrame)
     cv2.imshow('MOG', mogMask)
-    cv2.imshow('MOG2', mog2Mmask)
 
     cv2.imshow('countMOGMask', countMOGMask)
     cv2.imshow('morphMOGMask', morphMOGMask)
 
     cv2.moveWindow('Original', 100, 0)
     cv2.moveWindow('MOG', 500, 0)
-    cv2.moveWindow('MOG2', 900, 0)
-
-    cv2.moveWindow('countMOGMask', 400, 500)
-    cv2.moveWindow('morphMOGMask', 800, 500)
+    cv2.moveWindow('countMOGMask', 900, 0)
+    cv2.moveWindow('morphMOGMask', 1300, 0)
 
     k = cv2.waitKey(0) & 0xff
     # Enter for next frame and Esc for break the loop
